e4e6150132b09234446c83e167acfc4dd0a53701
yunusdemirboga <yunuseren179@gmail.com>
2026-05-08 21:20:26 +0300

    removed the TURN server

--- src/components/LiveViewModal.jsx
+++ src/components/LiveViewModal.jsx
@@ -5,15 +5,6 @@ const WS_URL = 'wss://se496-capstone-dashboard-backend.onrender.com/ws/webrtc/vi
 const ICE_SERVERS = [
   { urls: 'stun:stun.l.google.com:19302' },
   { urls: 'stun:stun1.l.google.com:19302' },
-  {
-    urls: [
-      'turn:openrelay.metered.ca:80',
-      'turn:openrelay.metered.ca:443',
-      'turns:openrelay.metered.ca:443',
-    ],
-    username:   'openrelayproject',
-    credential: 'openrelayproject',
-  },
 ]
 
 const STATUS = {
